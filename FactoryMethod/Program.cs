a69d7434adc2be93bb4de47f4139d24be123581c
cashwu <cash@cashwu.com>
2013-10-29 00:18:01 +0800

    FirstCommit

---
+++ FactoryMethod/Program.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethod
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+        }
+    }
+
+    public class Hamburg
+    {
+        internal void prepare()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void cook()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void assembly()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void box()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class PorkHamburg : Hamburg
+    {
+    }
+
+    public class BeefHamburg : Hamburg
+    {
+    }
+
+    public class SweetPorkHamburg : Hamburg
+    {
+    }
+
+    public class SweetBeefHamburg : Hamburg
+    {
+    }
+
+    public abstract class HamburgStore
+    {
+        private Hamburg orderHamburg(string type)
+        {
+            Hamburg ham = null;
+            ham = createHamburg(type);
+            ham.prepare();
+            ham.cook();
+            ham.assembly();
+            ham.box();
+            return ham;
+        }
+
+        public abstract Hamburg createHamburg(string type);
+    }
+
+    public class TaipeiHamburgStore : HamburgStore
+    {
+        public override Hamburg createHamburg(string type)
+        {
+            Hamburg ham = null;
+            switch (type)
+            {
+                case "pork":
+                    ham = new PorkHamburg();
+                    break;
+
+                case "beef":
+                    ham = new BeefHamburg();
+                    break;
+
+                default:
+                    throw new Exception();
+                    break;
+            }
+
+            return ham;
+        }
+    }
+
+    public class TainanHamburgStore : HamburgStore
+    {
+        public override Hamburg createHamburg(string type)
+        {
+            Hamburg ham = null;
+            switch (type)
+            {
+                case "pork":
+                    ham = new SweetPorkHamburg();
+                    break;
+
+                case "beef":
+                    ham = new SweetBeefHamburg();
+                    break;
+
+                default:
+                    throw new Exception();
+                    break;
+            }
+
+            return ham;
+        }
+    }
+}
---
+++ FactoryMethodTests/HamburgStoreTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FactoryMethod;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FactoryMethod.Tests
+{
+    [TestClass()]
+    public class HamburgStoreTests
+    {
+        [TestMethod()]
+        public void createHamburgTest()
+        {
+            HamburgStore store = new TainanHamburgStore();
+            Hamburg ham = store.createHamburg("pork");
+            Assert.AreEqual(typeof(SweetPorkHamburg), ham.GetType());
+
+            ham = store.createHamburg("beef");
+            Assert.AreEqual(typeof(SweetBeefHamburg), ham.GetType());
+
+            try
+            {
+                ham = store.createHamburg("test");
+                Assert.Fail();
+            }
+            catch (Exception)
+            {
+            }
+        }
+    }
+}
